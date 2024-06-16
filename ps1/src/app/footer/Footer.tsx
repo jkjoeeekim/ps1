77d78ad7c983cee48fc33754364079fc89cee854
Joe <jkjoeeekim@live.com>
2024-06-16 00:50:09 -0700

    update navbar and footer

--- ps1/src/app/footer/Footer.tsx
+++ ps1/src/app/footer/Footer.tsx
@@ -1,18 +1,25 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { CiInstagram } from "react-icons/ci";
+import { IconContext } from "react-icons";
+
+
+
 
 const Footer = () => {
   return (
-    <footer className='bg-base-300 h-20 text-black'>
-      <div className='navbar flex flex-row place-content-center h-20 max-w-screen-2xl m-auto'>
-        <div className='flex flex-col justify-items-center'>
-          <Link href='/' className='btn btn-ghost'>
+    <footer className='h-min text-black p-8 bg-[#f7f4ed]'>
+      <div className='flex flex-row place-content-center'>
+        <div className='flex flex-col place-content-center'>
+          <Link href="/" className='place-content-center w-min m-auto'>
+            <IconContext.Provider value={{ color: "", className: "", size:"1.8em" }}>
+              <CiInstagram></CiInstagram>
+            </IconContext.Provider>
+          </Link>
+          <Link href='/' className='btn btn-ghost tracking-widest mt-7'>
             ARGHAVAN CONCEPTS 2024
           </Link>
-          <Link href='/team' className='btn btn-ghost justify-self-center'>Some legal info</Link>
-          <FontAwesomeIcon icon="facebook" />       
-          <Link href='/contact' className='btn btn-ghost'>Contact Us</Link>
         </div>
         <div className=''>
         </div>
--- ps1/src/app/navbar/Navbar.tsx
+++ ps1/src/app/navbar/Navbar.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+
+// ** NAVIGATION BAR CLASSES ** //
+const Header = "sticky top-0 z-10 p-8 text-black bg-[#f7f4ed] place-content-between h-min max-w-screen-2xl flex m-auto";
+const NavbarMainLink = "btn btn-ghost p-8 hover:bg-slate-500 border-r-slate-500 border-r-1 rounded-sm text-3xl tracking-wide";
+const NavbarSideLinks = "btn btn-ghost pb-10 pt-10 pl-4 pr-4 hover:bg-slate-500 rounded-sm";
 
 const Navbar = () => {
-  var h = 'h-16';
   return (
-    <div className={`sticky top-0 z-10 h-16 m-8 text-black`}>
-      <div className={`navbar place-content-between h-16 max-w-screen-2xl flex m-auto`}>
-        <div className=''>
-          <Link href='/' className='btn btn-ghost font-bold hover:bg-slate-500 border-r-slate-500 border-r-1 rounded-sm text-3xl tracking-wide'>
-            ARGHAVAN CONCEPTS
-          </Link>
-        </div>
-        <div className=''>
-          <Link href='/' className='btn btn-ghost font-bold hover:bg-slate-500 rounded-sm'>
-            PROJECTS
-          </Link>
-          <Link href='/mission' className='btn btn-ghost font-bold hover:bg-slate-500 rounded-sm'>
-            PROCESS
-          </Link>
-          <Link href='/feed' className='btn btn-ghost font-bold hover:bg-slate-500 rounded-sm'>
-            ABOUT
-          </Link>
-          <Link href='/team' className='btn btn-ghost font-bold hover:bg-slate-500 rounded-sm'>
-            CONTACT
-          </Link>
-        </div>
-      </div>
-    </div>
+    <header className={Header}>
+      <nav className='h-min mt-auto mb-auto'>
+        <Link href='/' className={NavbarMainLink}>
+          ARGHAVAN CONCEPTS
+        </Link>
+      </nav>
+      <nav className='h-min mt-auto mb-auto'>
+        <Link href='/' className={NavbarSideLinks}>
+          PROJECTS
+        </Link>
+        <Link href='/' className={NavbarSideLinks}>
+          PROCESS
+        </Link>
+        <Link href='/' className={NavbarSideLinks}>
+          ABOUT
+        </Link>
+        <Link href='/' className={NavbarSideLinks}>
+          CONTACT
+        </Link>
+      </nav>
+    </header>
   )
 }
 
