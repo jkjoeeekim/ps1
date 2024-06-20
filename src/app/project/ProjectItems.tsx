1c1a5d5af24e685a985fe0d9a54053be9e9174f2
Joe <jkjoeeekim@live.com>
2024-06-19 18:46:48 -0700

    update project items

--- src/app/project/ProjectItems.tsx
+++ src/app/project/ProjectItems.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+
+import ProjectItem from './ProjectItem';
 
 import image1 from '../assets/ps1.jpg';
 import image2 from '../assets/ps2.jpeg';
@@ -21,79 +24,79 @@ const ProjectItems = () => {
   const rowClassFirst = `flex flex-row w-screen p-7 h-[${thirdX * 1.3}px]`;
   const rowClass = `flex flex-row w-screen px-7 pb-7 h-[${thirdX * 1.3}px]`;
   const itemClassFirst = 'project__item opacity-0 bg-[#fbf9f9] aspect-4/5 w-full h-full relative';
-  const itemClass = 'project__item opacity-0 bg-[#fbf9f9] ml-3 aspect-4/5 w-full h-full relative';
+  const itemClass = 'project__item opacity-0 bg-[#fbf9f9] ml-7 aspect-4/5 w-full h-full relative';
   const imageClass = 'w-full h-full hover:bg-slate-200 duration-300 ease-in-out ';
   const blackBgClass = 'absolute w-full h-full opacity-0 hover:opacity-90 duration-300 ease-in-out cursor-pointer';
-  const centerTextClass = 'absolute w-full h-full text-xl tracking-widest font-extrabold text-white text-center top-1/2';
+  const centerTextClass = 'absolute w-full h-1/2 text-xl tracking-widest font-extrabold text-white text-center top-1/2';
 
   return (
     <nav className='flex flex-col w-screen'>
       <nav className={rowClassFirst}>
         <section className={itemClassFirst}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image1} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image2} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image3} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
       </nav>
       <nav className={rowClass}>
         <section className={itemClassFirst}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image2} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image3} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image1} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
       </nav>
       <nav className={rowClass}>
         <section className={itemClassFirst}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image3} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image1} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
         <section className={itemClass}>
-          <div className={blackBgClass}>
+          <Link className={blackBgClass} href=''>
             <h3 className={centerTextClass}>Palo Alto Residence</h3>
             <Image src={blackBg} alt='Pic1' className='w-full h-full' style={{ objectFit: "cover" }}></Image>
-          </div>
+          </Link>
           <Image src={image2} alt='Pic1' className={imageClass} style={{ objectFit: "cover" }}></Image>
         </section>
       </nav>
