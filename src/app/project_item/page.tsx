951ca7782f36d2c6ecf981ad002921dd4f1baad7
Joe <jkjoeeekim@live.com>
2024-06-21 03:29:17 -0700

    add bang operator

--- src/app/project_item/page.tsx
+++ src/app/project_item/page.tsx
@@ -19,35 +19,35 @@ const ProjectItem = () => {
     'https://aops1.s3.us-west-1.amazonaws.com/',
     '.jpg',
   ]
+  var galleryLeftLinks = [];
+  var galleryRightLinks = [];
 
   const searchParams = useSearchParams();
   console.log(searchParams.get('project'));
   const projectName = searchParams.get('project');
   var projectNameArray = [''];
-  if (projectName !== "null" ) {
-    projectNameArray = projectName.split(" ");
+  if (typeof projectName !== "undefined" ) {
+    projectNameArray = projectName!.split(" ");
 
-  }
-  var projectInitials = '';
-  const projectImageCount = 28;
-
-  var galleryLeftLinks = [];
-  var galleryRightLinks = [];
-  
-  projectNameArray.forEach((element: String) => {
-    const lowerCaseInitial = element[0].toLowerCase();
-    projectInitials += lowerCaseInitial;
-  });
-
-  for (let i = 1; i <= projectImageCount; i++) {
-    if (i % 2 == 0) {
-      galleryLeftLinks.push(
-        <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={x} height={100} className='' style={{ objectFit: "cover" }}></Image>
-      )
-    } else {
-      galleryRightLinks.push(
-        <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={x} height={100} className='' style={{ objectFit: "cover" }}></Image>
-      )
+    var projectInitials = '';
+    const projectImageCount = 28;
+    
+    
+    projectNameArray.forEach((element: String) => {
+      const lowerCaseInitial = element[0].toLowerCase();
+      projectInitials += lowerCaseInitial;
+    });
+    
+    for (let i = 1; i <= projectImageCount; i++) {
+      if (i % 2 == 0) {
+        galleryLeftLinks.push(
+          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={x} height={100} className='' style={{ objectFit: "cover" }}></Image>
+        )
+      } else {
+        galleryRightLinks.push(
+          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={x} height={100} className='' style={{ objectFit: "cover" }}></Image>
+        )
+      }
     }
   }
   console.log()
