a710337b922f3ac36d5c3ac4cb7dbb4ce79a08e7
Joe <jkjoeeekim@live.com>
2024-06-21 04:35:19 -0700

    update project item

--- src/app/navbar/Navbar.tsx
+++ src/app/navbar/Navbar.tsx
@@ -18,8 +18,8 @@ const myFontBolder = localFont({ src: "../../sinkin-sans/SinkinSans-500Medium.ot
 
 // ** NAVIGATION BAR CLASSES ** //
 const Header = "text-[#110100] bg-[#cabcac] place-content-between h-[75px] flex m-auto tracking-wide";
-const NavbarMainLink = "btn btn-ghost px-4 tracking-widest text-4xl pt-7 rounded-sm hover:pt-3 hover:bg-[#fbf9f9] duration-500 ease-in-out";
-const NavbarSideLinks = `${myFontBold.className} btn btn-ghost px-4 text-lg tracking-widest pt-10 rounded-sm hover:pt-3 hover:bg-[#fbf9f9] duration-500 ease-in-out`;
+const NavbarMainLink = "btn btn-ghost px-4 tracking-widest text-4xl pt-7 rounded-sm hover:pt-3 hover:bg-[#fbf9f9] hover:text-yellow-500 duration-500 ease-in-out";
+const NavbarSideLinks = `${myFontBold.className} btn btn-ghost px-4 text-lg tracking-widest pt-10 rounded-sm hover:pt-3 hover:text-yellow-600 hover:bg-[#fbf9f9] duration-500 ease-in-out`;
 const NavbarSocialLinks = `${myFontBold.className} btn btn-ghost px-4 tracking-widest pt-7 rounded-sm hover:pt-2 hover:bg-[#fbf9f9] duration-500 ease-in-out`;
 
 const Navbar = () => {
--- src/app/project_item/Pictures.tsx
+++ src/app/project_item/Pictures.tsx
@@ -32,26 +32,72 @@ const Pictures = () => {
       projectInitials += lowerCaseInitial;
     });
 
+    const rightImageClassName = 'pb-4 pr-4 pl-2'
+    const leftImageClassName = 'pb-4 pl-4 pr-2'
+
+    // ** CREATE IMAGE GALLERY WITH DYNAMIC LINK NAMES ** //
     for (let i = 1; i <= projectImageCount; i++) {
       if (i % 2 == 0) {
         galleryLeftLinks.push(
-          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={0} height={0} sizes='100vw' className='' style={{ width: '100%', height: 'auto' }}></Image>
+          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={0} height={0} sizes='100vw' className={leftImageClassName} style={{ width: '100%', height: 'auto' }}></Image>
         )
       } else {
         galleryRightLinks.push(
-          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={0} height={0} sizes='100vw' className='' style={{ width: '100%', height: 'auto' }}></Image>
+          <Image src={linkName[0] + projectInitials + i + linkName[1]} priority key={i} alt='works' width={0} height={0} sizes='100vw' className={rightImageClassName} style={{ width: '100%', height: 'auto' }}></Image>
         )
       }
     }
+
+    const infoItemClassName = '';
+
+    // ** THE INFORMATION SECTION ** //
+    galleryRightLinks.push(
+      <div key={369} className='pb-4 pr-4 pl-2 h-full w-full text-end flex flex-col justify-end'>
+        <div className='pt-4 text-md tracking-widest flex flex-row justify-end'>
+          <div className='pr-5'>
+            {projectName} Residence
+          </div>
+          <div className='text-yellow-500 border-l-2 pl-5 border-gray-600'>
+            PROJECT
+          </div>
+        </div>
+        <div className='pt-4 text-md tracking-widest flex flex-row justify-end'>
+          <div className='pr-5'>
+            Studio Ozmaie
+          </div>
+          <div className='text-yellow-500 border-l-2 pl-5 border-gray-600'>
+            INTERIORS
+          </div>
+        </div>
+        <div className='pt-4 text-md tracking-widest flex flex-row justify-end'>
+          <div className='pr-5'>
+            Boss Architecture Firm, Brian Loffman
+          </div>
+          <div className='text-yellow-500 border-l-2 pl-5 border-gray-600'>
+            ARCHITECT
+          </div>
+        </div>
+        <div className='pt-4 text-md tracking-widest flex flex-row justify-end'>
+          <div className='pr-5'>
+            Jenny Kardmon
+          </div>
+          <div className='text-yellow-500 border-l-2 pl-5 border-gray-600'>
+            PHOTOGRAPHER
+          </div>
+        </div>
+      </div>
+    )
   }
   return (
-    <div className='w-full flex flex-row'>
-      <section className='w-1/2 flex flex-col '>
-        {[...galleryLeftLinks]}
-      </section>
-      <section className='w-1/2 flex flex-col '>
-        {[...galleryRightLinks]}
-      </section>
+    <div className='w-full flex flex-col'>
+      <div className='w-full flex flex-row'>
+        <section className='w-1/2 flex flex-col pt-4'>
+          {[...galleryLeftLinks]}
+        </section>
+        <section className='w-1/2 flex flex-col pt-4'>
+          {[...galleryRightLinks]}
+        </section>
+      </div>
     </div>
   )
 }
